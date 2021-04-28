27ec6367dba815d4dbe458b095de7075c38343e9
KavinUran <kavincool16@gmail.com>
2021-04-28 14:30:18 +0500

    New Task

---
+++ src/test/java/org/test/GitHub.java
@@ -0,0 +1,30 @@
+package org.test;
+
+public class GitHub {
+
+	private void method1() {
+		System.out.println("method one");
+
+	}
+	
+	private void method2() {
+		System.out.println("method two");
+
+	}
+	
+	private void method3() {
+		System.out.println("method three");
+
+	}
+	
+	private void method4() {
+		System.out.println("method four");
+
+	}
+	
+	private void method5() {
+		System.out.println("method five");
+
+	}
+	
+}
